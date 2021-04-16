de183123a7b64c245754d39ad8abcb4863942a2f
mohamedahmed7 <moh.ahmed.abass@gmail.com>
2021-04-16 15:43:15 +0200

    login & register user done

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
-from .views import index, userView, createUser
+from .views import index, userView, createUser, loginUser
 
 urlpatterns = [
     path('', index),
     path('user/<int:user_id>', userView.as_view()),
-    path('register', createUser.as_view())
+    path('register', createUser.as_view()),
+    path('login', loginUser.as_view())
 ]
